cac0c841fbc5cb78ca541e9d569371b0924be216
Daniel Thompson <danthwa@gmail.com>
2016-12-19 20:51:55 -0800

    removed non-basic parts of the answer sheet

--- PeopleViewController.swift
+++ PeopleViewController.swift
@@ -14,79 +14,64 @@ class PeopleViewController: UITableViewController {
     var people = [String]()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-        // pepare request information
+        // prepare request information and session
         let url = NSURL(string: "http://swapi.co/api/people/")
         let session = URLSession.shared
         
-        // call each page of 'people' URL recursively
-        self.makePeopleRequests(url: url, session: session)
-    }
-    
-    func makePeopleRequests(url: NSURL?, session: URLSession){
         // retrieve data from url and handle with 'completionHandler' function enclosure
         let task = session.dataTask(with: url! as URL, completionHandler: {
+            // callback has three arguments
             data, response, error in
             do {
-                // Continue if we can cast response to dictionary type
+                // is there a result? Continue if we can cast response to dictionary type
                 if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
                     
-                    // helper function defined below -- takes response and
-                    // appends people names to the people array
-                    self.parsePeople(response: jsonResult)
-                    
-                    // this print statement shows each page url before request
-                    print("What's next? \(jsonResult["next"])")
-                    
-                    // if there is a url string at the 'next' key of our results page
-                    // set url for next page and recurse with new url
-                    if let nextPage = jsonResult["next"] as? String {
-                        let url = NSURL(string: nextPage)
-                        self.makePeopleRequests(url: url, session: session)
+                    // cast results key to array for iteration
+                    if let results = jsonResult["results"] as? NSArray {
+                        for person in results {
+                            // cast to dictionary for data extraction
+                            let personDict = person as! NSDictionary
+                            self.people.append(personDict["name"]! as! String)
+                        }
                     }
-                    
                 }
-                // Reload data inside asynchronous function to prevent loading blank data
+                // async -- if we loaded the data outside of the do-catch statement
+                // it would attempt to load before we had populated the 'people' array
                 self.tableView.reloadData()
             } catch {
-                print("Something went wrong")
+                print(error)
             }
         })
-        // execute request task at each stage of recursion
+        
         task.resume()
     }
     
-    // parse people casts results to array for iteration
-    func parsePeople(response: NSDictionary){
-        if let results = response["results"] as? NSArray {
-            for person in results {
-                let personDict = person as! NSDictionary
-                self.people.append(personDict["name"]! as! String)
-            }
-        }
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source: people
-
+    
+    // MARK: - Table view data source
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
         return people.count
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
         cell.textLabel?.text = people[indexPath.row]
         return cell
     }
-
+    
 }
