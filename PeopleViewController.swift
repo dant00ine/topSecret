c6fbc3a9d64e95a50a7422dc88930d30ead6d10c
Daniel Thompson <danthwa@gmail.com>
2016-12-19 19:50:06 -0800

    Get /Films/ basic assignment with answersheet comments

--- PeopleViewController.swift
+++ PeopleViewController.swift
@@ -9,16 +9,46 @@
 import UIKit
 
 class PeopleViewController: UITableViewController {
-
+    
+    // our tableView data -- see cellForRowAt function
+    var people = [String]()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        // prepare request information and session
+        let url = NSURL(string: "http://swapi.co/api/people/")
+        let session = URLSession.shared
+        
+        // retrieve data from url and handle with 'completionHandler' function enclosure
+        let task = session.dataTask(with: url! as URL, completionHandler: {
+            // callback has three arguments
+            data, response, error in
+            do {
+                // is there a result? Continue if we can cast response to dictionary type
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                    
+                    // cast results key to array for iteration
+                    if let results = jsonResult["results"] as? NSArray {
+                        for person in results {
+                            // cast to dictionary for data extraction
+                            let personDict = person as! NSDictionary
+                            self.people.append(personDict["name"]! as! String)
+                        }
+                    }
+                }
+                // async -- if we loaded the data outside of the do-catch statement
+                // it would attempt to load before we had populated the 'people' array
+                self.tableView.reloadData()
+            } catch {
+                print(error)
+            }
+        })
+        
+        task.resume()
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -29,23 +59,20 @@ class PeopleViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return people.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
+        cell.textLabel?.text = people[indexPath.row]
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
